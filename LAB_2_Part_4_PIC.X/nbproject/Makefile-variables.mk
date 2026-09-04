3f5a08625a3011ebb89dafc1cf2c1f10fa4aac47
groeluke <groeluke@isu.edu>
2026-09-03 21:56:00 -0600

    Written code for part 4 by using the same idea as used in part 3, this gave a good start to the code this is the first test code

---
+++ LAB_2_Part_4_PIC.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LAB_2_Part_4_PIC.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LAB_2_Part_4_PIC.X.production.hex
